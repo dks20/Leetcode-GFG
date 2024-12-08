4df69635437d4a3011cce2a53aaa7fd5be1af124
Deepak kumar singh <92033767+dks20@users.noreply.github.com>
2024-12-08 21:47:53 +0530

    Time: 2 ms (83.41%), Space: 18.6 MB (15.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,17 +1,39 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        mapp = {}
+        f = nums[0]
+        s = nums[0]
+
+        fc , sc =0,0
+
+        for num in nums:
+            if num == f:
+                fc +=1
+            elif num == s:
+                sc += 1
+            elif fc == 0:
+                f = num
+                fc +=1
+            elif sc == 0:
+                s = num
+                sc +=1
+            else:
+                fc -= 1
+                sc -= 1
+
+        fc = sc = 0
+        for num in nums:
+            if num == f:
+                fc += 1
+            elif num == s:
+                sc += 1
+
+        # Step 3: Return candidates with count > n // 3
+        result = []
         n = len(nums)
-        ans = []
+        if fc > n // 3:
+            result.append(f)
+        if sc > n // 3:
+            result.append(s)
+        return result
 
-        for i in nums:
-            if i in mapp:
-                mapp[i] +=1
-            else: 
-                mapp[i] =1
 
-        for key,values in mapp.items():
-            if values > n/3:
-                ans.append(key)
-        
-        return ans
