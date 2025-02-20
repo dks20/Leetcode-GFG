6890dc8b16723bf7b6e54e02ecd2f77e1f8d4c70
Deepak kumar singh <92033767+dks20@users.noreply.github.com>
2025-02-20 14:35:13 +0530

    Time: 407 ms (38.57%), Space: 17.8 MB (83.41%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        ans = []
+        n = len(nums)
+        nums.sort()
+
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1,n-2):
+                if j> i+1 and nums[j] == nums[j-1]:
+                    continue
+                k = j+1
+                l= n-1 
+
+                while k < l:
+                    tempsum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if tempsum == target:
+                        ans.append([nums[i], nums[j] , nums[k] , nums[l]])
+                        k+=1
+                        l-=1
+                        while k < l and nums[k] == nums[k-1]:
+                            k+=1
+                        while k < l and nums[l] == nums[l+1]:
+                            l-=1
+
+                    elif tempsum > target:
+                        l-=1
+                    else:
+                        k+=1
+
+        return ans
+        
