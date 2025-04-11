12ded27c6799cca63844a926c94ec3de45031e91
Deepak kumar singh <92033767+dks20@users.noreply.github.com>
2025-04-11 19:18:32 +0530

    Time: 9 ms (21.03%), Space: 17.8 MB (40.49%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
+        hmap = {"I" : 1,
+                "V" : 5,
+                "X" : 10,
+                "L" :50,
+                "C" : 100,
+                "D" : 500,
+                "M" :1000}
+
         ans = 0
-        
+
         for i in range(len(s)):
-            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
-                ans -= m[s[i]]
-            else:
-                ans += m[s[i]]
-        
+            if i+1 < len(s) and s[i] == "I" and (s[i+1] == "V" or s[i+1] == "X"):
+                ans -= hmap[s[i]]
+            elif i+1 < len(s) and s[i] == "X" and (s[i+1] == "L" or s[i+1] == "C"):
+                ans -= hmap[s[i]]
+            elif i+1 < len(s) and s[i] == "C" and (s[i+1] == "D" or s[i+1] == "M"):
+                ans -= hmap[s[i]]
+            else:  
+                ans += hmap[s[i]]
+
         return ans
+
         
